b018625e3fffa916615bddc374cf6bcd1c08fd65
V <V@DESKTOP-AUPSTRG>
2017-11-05 22:33:46 -0700

    Removed test

--- test/q8.sql
+++
@@ -1,5 +0,0 @@
--- Get item listings from an order
-
-select olines.sid as sid, stores.name as name, olines.pid as pid, products.name as name, olines.qty as qty, products.unit as unit, olines.uprice as unitprice
-from olines, stores, products
-where olines.oid == 1 and olines.sid == stores.sid and olines.pid == products.pid;
